dc938d77f9eb8819e6d19640c1a6f574216680d7
corybaird <31351628+corybaird@users.noreply.github.com>
2025-08-21 02:36:27 +0000

    Deploying to gh-pages from @ corybaird/corybaird.github.io@564077c7e9dd2574947ad86e23602cd56791b0cb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -69,6 +69,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/cory-baird-mn", "_blank");
         },
       },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0002-6600-1538", "_blank");
+        },
+      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
